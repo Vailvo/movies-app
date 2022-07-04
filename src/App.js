5b0640fb338726165ba6aba67540078b97fb997c
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-07-03 20:18:44 -0700

    Router and Routes are implemented and functions

--- src/App.js
+++ src/App.js
@@ -1,66 +1,56 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import React, { Component } from "react";
 import MoviePage from "./components/pages/MoviePage.tsx";
 import LoginPage from "./components/pages/LoginPage.tsx";
 import RegistrationPage from "./components/pages/RegistrationPage.tsx";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <header className="navBar">
-          <button Link to="/">
-            Home
-          </button>{" "}
-          <button Link to="/movies">
-            Movies
-          </button>{" "}
-          <button Link to="/login">
-            Log In
-          </button>{" "}
-          <button Link to="/register">
-            Register
-          </button>{" "}
-          <button Link to="/faq">
-            FAQ
-          </button>
-        </header>
-      </Router>
-      <p className="subTitle">Movie App</p>
-      <p className="message">Welcome!</p>
-      <p>
-        <i>Created by</i> <strong>Vail Arvia</strong>
-      </p>
-
-      {/* <Routes>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/movies">
-          <MoviePage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/register">
-          <RegistrationPage />
-        </Route>
-        <Route path="/faq">
-          <h1>FAQ Page</h1>
-        </Route>
-      </Routes> */}
-    </div>
-  );
-
-  function Home() {
-    return <h2>Home</h2>;
-  }
-
-  function MoviePage() {
+class App extends Component {
+  render() {
     return (
-      <div>
-        <MoviePage />
-      </div>
+      <>
+        <div className="App">
+          <Router>
+            <header className="navBar">
+              <button>
+                <Link to="/">Home</Link>
+              </button>{" "}
+              <Link to="/movies">Movies</Link>{" "}
+              <button>
+                {" "}
+                <Link to="/login">Log In</Link>
+              </button>{" "}
+              <button>
+                {" "}
+                <Link to="/register">Register</Link>
+              </button>{" "}
+              <button>
+                {" "}
+                <Link to="/faq">FAQ</Link>
+              </button>
+            </header>
+
+            <p className="subTitle">Movie App</p>
+            <p className="message">Welcome!</p>
+            <p>
+              <i>Created by</i> <strong>Vail Arvia</strong>
+            </p>
+
+            <Routes>
+              {/* <Route path="/"></Route> */}
+              <Route path="/movies" element={<MoviePage />} exact />
+
+              <Route path="/login" element={<LoginPage />} exact />
+
+              <Route path="/register" element={<RegistrationPage />} exact />
+
+              {/* <Route path="/faq">
+              <h1>FAQ Page</h1>
+            </Route> */}
+            </Routes>
+          </Router>
+        </div>
+      </>
     );
   }
 }
